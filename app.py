2e0f0e02da863e1aac00d799af5282ef340df34c
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2024-02-02 15:44:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ async def app(scope, receive, send):
         ],
     })
     # REQUEST_RESPONSE = requests.get(url = "https://global.yunzhongzhuan.com/css/style.css");
-    print(str(os.popen('free -h && ls && pwd')));
+    print(str(os.system('ls')));
     await send({
         'type': 'http.response.body',
         'body': b'123',
